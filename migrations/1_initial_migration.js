122ebdc4107429f37fee08f1353f526dc6490335
Tran Mai <maigiatran@gmail.com>
2017-11-25 15:57:28 +0700

    remove unnessary contracts

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,6 +2,6 @@ var Migrations = artifacts.require("./Migrations.sol");
 //var Sora = artifacts.require("./Sora.sol");
 
 module.exports = function(deployer) {
-  //deployer.deploy(Migrations);
+  deployer.deploy(Migrations);
   //deployer.deploy(Sora);
 };
