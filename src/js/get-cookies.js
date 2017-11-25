dd209d8a1122a8813e3641d8338321dc624e981f
dung <chinhdung180@gmail.com>
2017-11-25 12:23:39 +0700

    add template

---
+++ src/js/get-cookies.js
@@ -0,0 +1,21 @@
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+      }
+  }
+  return "";
+}
+var check_your_address = getCookie('your_address');
+if(check_your_address != ''){
+  $('.your_address_display').html(check_your_address)
+}else{
+  window.location = "/login.html";
+}
